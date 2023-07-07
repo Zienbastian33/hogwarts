7411dbc72209f77829c62ebb2b89677389417fd7
zienbastian33 <bastian.chacon93@gmail.com>
2023-07-06 23:02:29 -0400

    configurando deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,13 @@ lock "~> 3.17.3"
 set :application, "myapp"
 set :repo_url, "git@github.com:zienbastian33/hogwarts.git"
 
+set :user, 'ubuntu'
+set :ssh_options, {
+    forward_agent: true,
+    auth_methods: ['publickey'],
+    keys: ['~/.ssh/rails_aws.pem']
+}
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
